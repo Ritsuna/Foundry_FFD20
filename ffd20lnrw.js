379fae587760427e98f5d2082675c847f60f0b7d
Ritsuna <36092027+Ritsuna@users.noreply.github.com>
2021-02-03 23:12:21 -0800

    Error Correction to 0.0.10

--- ffd20lnrw.js
+++ ffd20lnrw.js
@@ -206,7 +206,7 @@ Hooks.once("setup", function () {
  */
 Hooks.once("ready", async function () {
   // Migrate data
-  const NEEDS_MIGRATION_VERSION = "0.0.9";
+  const NEEDS_MIGRATION_VERSION = "0.0.10";
   let PREVIOUS_MIGRATION_VERSION = game.settings.get("ffd20lnrw", "systemMigrationVersion");
   if (typeof PREVIOUS_MIGRATION_VERSION === "number") {
     PREVIOUS_MIGRATION_VERSION = PREVIOUS_MIGRATION_VERSION.toString() + ".0";
